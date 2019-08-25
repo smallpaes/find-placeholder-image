5d530ac3cdb98f23425d19dd110a49ee0b2cf4a8
Mike <mike.huang.mikank@gmail.com>
2019-08-25 17:53:03 +0800

    Refactor content.js, add reload button, error message, and tooltip

--- content.js
+++ content.js
@@ -1,65 +1,107 @@
-(function () {
+window.onload = function () {
   let clickedImg = null
 
-  document.addEventListener('contextmenu', event => {
-    clickedImg = event.target
-  })
+  // generate a table for each format
+  function generateTableRow(size, url) {
+    return `
+      <tr>
+        <td>${size}</td>
+        <td class="url">
+          <span class="link">${url}</span>
+          <span class="tooltip-text">Click to copy</span>
+        </td>
+      </tr>
+    `
+  }
+
+  // access image from Pixabay , Pexels, Unsplash 
+  function generateSrcsetImages() {
+    const images = clickedImg.srcset.split(', ')
+    return images.map(image => {
+      const url = image.split(' ')[0]
+      const size = image.split(' ')[1]
+      return generateTableRow(size, url)
+    }).join(' ')
+  }
+
+  // access image from Picjumbo or others
+  function generateSrcImage() {
+    const size = clickedImg.width
+    const url = clickedImg.src
+    return generateTableRow(size, url)
+  }
+
+  function generateTbody() {
+    // generate table rows based on where the links are stored
+    const tbodyInnerHTML = clickedImg.srcset ? generateSrcsetImages() : generateSrcImage()
+    return tbodyInnerHTML
+  }
+
+  function generateTable(tbodyInnerHTML) {
+    // replace the page with a created table containing links
+    document.querySelector('body').innerHTML = `
+      <section>
+        <div class="table-container mt-4 p-0 rounded-lg">
+          <table class="table mb-0">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">Size</th>
+                <th scope="col">Link</th>
+              </tr>
+            </thead>
+            <tbody class="text-secondary">
+              ${tbodyInnerHTML}
+            </tbody>
+          </table>
+        </div>
+        <!--Reload Button-->
+        <div class="button-panel">
+          <button id="reload">Back to photo</button>
+        </div>
+      </section>
+    `
+  }
 
   function copyUrl() {
+    const link = event.target.closest('.url').firstElementChild
     const selection = window.getSelection();
     const range = document.createRange();
-    range.selectNodeContents(event.target);
+    range.selectNodeContents(link);
     selection.removeAllRanges();
     selection.addRange(range);
     document.execCommand('copy');
     selection.removeAllRanges();
   }
 
-  // listen to message request from the extension: background.js
-  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    // check if the message request is to gerUrl
-    if (!request.getUrl) { return }
-
-    // get all urls of the image chosen
-    const images = !clickedImg.srcset ? clickedImg.src.split() : clickedImg.srcset.split(',')
-    let tbodyInnerHTML = ''
-
-    // format the links for table use
-    images.forEach(image => {
-      const url = image.split(' ')[0] ? image.split(' ')[0] : image.split(' ')[1]
-      const size = image.split(' ').length === 1 ? 'Single Size'
-        : image.split(' ')[0] ? image.split(' ')[1]
-          : image.split(' ')[2]
-
-      tbodyInnerHTML += `
-        <tr>
-          <td>${size}</td>
-          <td class='url'>
-            ${url}
-          </td>
-        </tr>
-      `
-    })
-
-    // replace the page with a created table containing links
-    document.querySelector('body').innerHTML = `
-      <table>
-        <thead>
-          <tr> 
-            <th><h3>Size</h3></th>
-            <th><h3>Link</h3></th>
-          </tr>
-        <thead>
-        <tbody>
-          ${tbodyInnerHTML}
-        </tbody>
-      </table>
+  function showErrorMessage() {
+    const errorMessage = `
+      <div class="alert-message">
+        <span class="closebtn">&times;</span> 
+        Something went wrong. Please try again on the image!
+      </div>
     `
+    // insert alert message to the top of the page
+    document.querySelector('body').insertAdjacentHTML('afterbegin', errorMessage)
+    // close the message when clicking the close button
+    document.querySelector('.closebtn').addEventListener('click', event => event.target.parentElement.remove())
+  }
 
+  function generateReport(request, sender, sendResponse) {
+    // show error message if getUrl is false or clickImg is null
+    if (!request.getUrl || !clickedImg) { return showErrorMessage() }
+    // generate tbody
+    const tbodyInnerHTML = generateTbody()
+    // generate the report with tbody
+    generateTable(tbodyInnerHTML)
+    // listen to url click to copy url
+    document.querySelectorAll('.url').forEach(link => link.addEventListener('click', copyUrl))
+    // listen to reload button click to lead user back to image page
+    document.getElementById('reload').addEventListener('click', () => window.location.reload())
+  }
 
-    document.querySelectorAll('.url').forEach(link => {
-      link.addEventListener('click', copyUrl)
-    })
-  })
-})()
+  // listen to contextmenu being opened and save the target image
+  document.addEventListener('contextmenu', event => clickedImg = event.target)
 
+  // listen to message request from the extension: background.js
+  chrome.runtime.onMessage.addListener(generateReport)
+}
